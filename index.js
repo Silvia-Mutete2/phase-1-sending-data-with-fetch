2f1f7156ca9f05b617c56cdd23a4cab9bb84c740
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-06-17 14:52:06 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+
+function submitData(name, email){
+  return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+      },
+      body: JSON.stringify({
+          name:name,
+          email:email,
+      }),
+  }) 
+    .then(resp =>resp.json()) 
+    .then(obj => document.body.innerHTML = object.id ) 
+    .catch(err => document.body.innerHTML=err.message) 
+    } 
